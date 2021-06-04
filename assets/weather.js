b5ab0aae379ef22a16555295d3aba2909f8b856e
rmwillow <rmwillowbis2bis@gmail.com>
2021-06-04 16:54:52 -0500

    one city show after search

--- assets/weather.js
+++ assets/weather.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
-    $('#submit').click(function() {
+    $('#add-city').click(function() {
         var city = $("#searchCity").val();
+        console.log(city);
+
+        citySearch = [''];
+        citySearch.push(city);
+        console.log(citySearch);
+
+
+
+
+
+
         //check field is not empty
         if (city != '') {
 
@@ -47,7 +58,7 @@ $(document).ready(function() {
                                 // console.log(fiveDateEpoch, index);
                                 var fiveDayTemp = "Temperature: " + daySet.temp.day,
                                     index;
-                                // console.log(fiveDayTemp);
+                                console.log(fiveDayTemp);
 
 
                                 var fiveDayHumid = "Humidity: " + daySet.humidity,
@@ -65,50 +76,58 @@ $(document).ready(function() {
                                     console.log(weatherDay);
                                     var fiveDayDesc = "Description: " + weatherDay.description,
                                         index;
-                                    var fiveDayIcon = "ICON: " + weatherDay.icon,
+                                    var fiveDayImage = weatherDay.icon,
                                         index;
-                                    // console.log(fiveDayDesc, fiveDayIcon);
+                                    // img.src = 'http://openweathermap.org/img/wn/' + fiveDayImage + '.png';
 
-                                    var fiveDayForcast = ['']
+                                    // fiveDayIcon = 'http://openweathermap.org/img/wn/' + fiveDayImage + '.png';
+                                    // console.log(fiveDayIcon);
 
-                                    fiveDayForcast.push(fiveDayTemp, fiveDayHumid, fiveDayWind, fiveDayDesc, fiveDayIcon);
+
+                                    var fiveDayIcon = `http://webpage.com/images/'${fiveDayImage, index}.png`,
+                                        index;
+
+
+
+
+                                    var fiveDayForcast = [''];
+
+                                    fiveDayForcast.push(fiveDateEpoch, fiveDayTemp, fiveDayHumid, fiveDayWind, fiveDayDesc, fiveDayIcon);
                                     console.log(fiveDayForcast);
+
+                                    localStorage.setItem(city, fiveDayForcast);
+
 
                                     (function() {
                                         var ul = document.createElement('ul');
-                                        ul.setAttribute('id', 'proList');
+                                        ul.setAttribute('id', 'forecastList');
+                                        fiveForecast = fiveDayForcast;
 
-                                        productList = fiveDayForcast;
 
-                                        document.getElementById('renderList').appendChild(ul);
-                                        productList.forEach(renderProductList);
+                                        document.getElementById('renderForecastList').appendChild(ul);
+
+                                        fiveForecast.forEach(renderProductList);
+                                        // iconDay.forEach(renderProductList);
+
 
                                         function renderProductList(element, index, arr) {
                                             var li = document.createElement('li');
                                             li.setAttribute('class', 'item');
 
+
                                             ul.appendChild(li);
 
                                             li.innerHTML = li.innerHTML + element;
+                                            document.getElementById("forecastList").style.backgroundColor = "blue";
+
                                         }
                                     })();
 
                                 })
-
-
                             });
 
 
-
                         }
-
-
-
-
-
-
-
-
 
                         //for loop on five days
 
@@ -116,13 +135,79 @@ $(document).ready(function() {
                         //create elements 
 
 
-
-
-
-
                     });
                 }
             })
         }
+
+
+
+        function renderTodos(city) {
+            // Empties out the html
+            $('#cityItem').empty();
+            console.log(city);
+
+            // Creates a new variable 'toDoItem' that will hold a "<p>" tag
+            // Sets the `list` item's value as text of this <p> element
+            var toDoItem = $('<p>');
+            toDoItem.text(city);
+
+            // Creates a button `toDoClose` with an attribute called `data-to-do` and a unique `id`
+            var toDoClose = $('<button>');
+            toDoClose.attr('data-to-do', city);
+
+            // Gives the button a class called 'checkbox'
+            toDoClose.addClass('checkbox');
+
+            // Adds a checkmark symbol as its text value
+            toDoClose.text('X');
+
+            // Adds the button to the `toDoItem`
+            toDoItem = toDoItem.prepend(toDoClose);
+
+            // Adds 'toDoItem' to the To-Do city div
+            $('#cityItem').append(toDoItem);
+
+        }
+
+        $('#add-city').on('click', function(event) {
+            event.preventDefault();
+
+            // Get the to-do "value" from the textbox and store it as a variable using `.val()` and `.trim()`
+            var toDoTask = $('#searchCity')
+                .val()
+                .trim();
+
+            // Add the new to-do to our local 'city' variable
+            city.push(toDoTask);
+
+            // Update the cityItem on the page
+            renderTodos(city);
+
+            // Save the cityItem into localStorage
+            // We need to use JSON.stringify to turn the city from an array into a string
+            localStorage.setItem('city-item', JSON.stringify(city));
+
+            // Clear the textbox when done using `.val()`
+            $('#searchCity').val('');
+        });
+
+        $(document).on('click', '.checkbox', function() {
+            // Get the `id` of the button from its data attribute and hold in a variable called 'toDoNumber'
+            var toDoNumber = $(this).attr('data-to-do');
+
+            // Delete the to-do with that `id` from our local `city` using `.splice()`
+            city.splice(toDoNumber, 1);
+
+            // Update the cityItem on the page
+            renderTodos(city);
+
+            // Save the cityItem into localStorage
+            // We need to use JSON.stringify to turn the city from an array into a string
+            localStorage.setItem('cityNameList', JSON.stringify(city));
+        });
+
+        // render our cityItem on page load
+        renderTodos(city);
     })
 });
